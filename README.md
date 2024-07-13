74dc531783a2aed077d1edcf9523160724a66a9f
Cristiano Sobral <cssobral2013@gmail.com>
2024-07-12 22:04:12 -0300

    Update version 2.0

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 # Cheyenne Sans
 Current font files for the Cheyenne Sans typeface.
-This typeface is based off of https://github.com/impallari/Libre-Franklin and https://github.com/uswds/public-sans
+This typeface is based off of:
+https://github.com/impallari/Libre-Franklin
+https://github.com/uswds/public-sans
+https://github.com/googlefonts/roboto-flex
+https://github.com/barrymieny/general-gothic
 
 ## License
 - Cheyenne Sans is licensed under the SIL Open Font License v1.1 (http://scripts.sil.org/OFL)
@@ -11,3 +15,51 @@ This typeface is based off of https://github.com/impallari/Libre-Franklin and ht
 - v17: normalized vertical spacing across all weights and styles
 - v16: added italics and `:18F:` ligature glyph
 - v15: add tabular figures to roman styles
+
+## Cheyenne Sans changelog
+
+[July 12, 2024] Version 2.0
+
+ - Imported support for:
+   - Figures (0-9) from General Gothic
+   - Greek/Cyrillic from Roboto Flex
+   - Latin/Vietnamese from 18Franklin (now currently known as Public Sans)
+   
+   
+## Acknowledgements
+
+If you make modifications be sure to add your name (N),
+email (E), web-address (if you have one) (W) and
+description (D). This list is in alphabetical order.
+
+N: David Berlow
+E: none
+W: https://github.com/googlefonts/roboto-flex
+D: Portions from Roboto Flex
+
+N: Pablo Impallari
+E: impallari@gmail.com
+W: http://www.impallari.com
+D: Designer
+
+N: Rodrigo Fuenzalida
+E: fuenzarodrigo@gmail.com
+W: http://www.impallari.com
+D: Designer
+
+N: Barry Mieny
+E: none
+W: https://barry.mieny.com/
+D: Portions from General Gothic
+
+N: Cristiano Sobral
+E: cssobral2013@gmail.com
+W: https://x.com/@CrisSobral1995/
+D: Main changes
+
+N: Dan O. Williams
+E: daniel.williams@gsa.gov
+W: https://designsystem.digital.gov
+D: Designer
+
+
---
+++ generate-woff-woff2.py
@@ -0,0 +1,303 @@
+from fontTools.ttLib import TTFont
+
+'''Generate WOFF from TTF or OTF font.'''
+
+srcPath  = './ttf/CheyenneSans-Thin.ttf'
+woffPath = './web/CheyenneSans-Thin.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/CheyenneSans-ThinItalic.ttf'
+woffPath = './web/CheyenneSans-ThinItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/CheyenneSans-ExtraLight.ttf'
+woffPath = './web/CheyenneSans-ExtraLight.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+
+srcPath  = './ttf/CheyenneSans-ExtraLightItalic.ttf'
+woffPath = './web/CheyenneSans-ExtraLightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)
+    
+srcPath  = './ttf/CheyenneSans-Light.ttf'
+woffPath = './web/CheyenneSans-Light.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CheyenneSans-LightItalic.ttf'
+woffPath = './web/CheyenneSans-LightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CheyenneSans-LightItalic.ttf'
+woffPath = './web/CheyenneSans-LightItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CheyenneSans-Regular.ttf'
+woffPath = './web/CheyenneSans-Regular.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CheyenneSans-Italic.ttf'
+woffPath = './web/CheyenneSans-Italic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CheyenneSans-Medium.ttf'
+woffPath = './web/CheyenneSans-Medium.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CheyenneSans-MediumItalic.ttf'
+woffPath = './web/CheyenneSans-MediumItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CheyenneSans-SemiBold.ttf'
+woffPath = './web/CheyenneSans-SemiBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CheyenneSans-SemiBoldItalic.ttf'
+woffPath = './web/CheyenneSans-SemiBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CheyenneSans-SemiBold.ttf'
+woffPath = './web/CheyenneSans-SemiBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CheyenneSans-SemiBoldItalic.ttf'
+woffPath = './web/CheyenneSans-SemiBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CheyenneSans-Bold.ttf'
+woffPath = './web/CheyenneSans-Bold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CheyenneSans-BoldItalic.ttf'
+woffPath = './web/CheyenneSans-BoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CheyenneSans-ExtraBold.ttf'
+woffPath = './web/CheyenneSans-ExtraBold.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+
+    
+srcPath  = './ttf/CheyenneSans-ExtraBoldItalic.ttf'
+woffPath = './web/CheyenneSans-ExtraBoldItalic.woff'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff'
+    font.save(woffPath)    
+ 
+
+
+
+'''Generate WOFF2 from TTF or OTF font.'''
+
+srcPath  = './ttf/CheyenneSans-Thin.ttf'
+woff2Path = './web/CheyenneSans-Thin.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/CheyenneSans-ThinItalic.ttf'
+woff2Path = './web/CheyenneSans-ThinItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/CheyenneSans-ExtraLight.ttf'
+woff2Path = './web/CheyenneSans-ExtraLight.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+
+srcPath  = './ttf/CheyenneSans-ExtraLightItalic.ttf'
+woff2Path = './web/CheyenneSans-ExtraLightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)
+    
+srcPath  = './ttf/CheyenneSans-Light.ttf'
+woff2Path = './web/CheyenneSans-Light.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/CheyenneSans-LightItalic.ttf'
+woff2Path = './web/CheyenneSans-LightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/CheyenneSans-LightItalic.ttf'
+woff2Path = './web/CheyenneSans-LightItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/CheyenneSans-Regular.ttf'
+woff2Path = './web/CheyenneSans-Regular.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/CheyenneSans-Italic.ttf'
+woff2Path = './web/CheyenneSans-Italic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/CheyenneSans-Medium.ttf'
+woff2Path = './web/CheyenneSans-Medium.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/CheyenneSans-MediumItalic.ttf'
+woff2Path = './web/CheyenneSans-MediumItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/CheyenneSans-SemiBold.ttf'
+woff2Path = './web/CheyenneSans-SemiBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/CheyenneSans-SemiBoldItalic.ttf'
+woff2Path = './web/CheyenneSans-SemiBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/CheyenneSans-SemiBold.ttf'
+woff2Path = './web/CheyenneSans-SemiBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/CheyenneSans-SemiBoldItalic.ttf'
+woff2Path = './web/CheyenneSans-SemiBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/CheyenneSans-Bold.ttf'
+woff2Path = './web/CheyenneSans-Bold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/CheyenneSans-BoldItalic.ttf'
+woff2Path = './web/CheyenneSans-BoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/CheyenneSans-ExtraBold.ttf'
+woff2Path = './web/CheyenneSans-ExtraBold.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
+    
+srcPath  = './ttf/CheyenneSans-ExtraBoldItalic.ttf'
+woff2Path = './web/CheyenneSans-ExtraBoldItalic.woff2'
+
+with TTFont(srcPath) as font:
+    font.flavor = 'woff2'
+    font.save(woff2Path)    
+
